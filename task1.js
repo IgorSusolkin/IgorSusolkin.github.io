ef89a524fdc69c954ac3055b9b026764297e7f7f
Igor <ysusolkin@gmail.com>
2017-03-22 10:35:38 +0300

    add script with function add, sub, mul, div, floor

---
+++ task1.js
@@ -0,0 +1,40 @@
+function add(a, b) {
+  return a + b;
+}
+function sub(a, b) {
+  return a - b;
+}
+function mul(a, b) {
+  return a * b;
+}
+function div(a, b) {
+  return a / b;
+}
+function floor(a, b) {
+  return parseInt(a / b);
+}
+
+console.log("add");
+console.log(add(5,5));
+console.log(add(5,-4));
+console.log(add(5,"4"));
+console.log(add("-5","-4"));
+console.log(add(5,{}));
+console.log(add(5,true));
+console.log("sub");
+console.log(sub(5,3));
+console.log(sub(5,"-3"));
+console.log(sub("5",true));
+console.log(sub(5,{}));
+console.log("mul");
+console.log(mul("2","6"));
+console.log(mul(2,"-6"));
+console.log(mul(2,""));
+console.log("div");
+console.log(div(5,0));
+console.log(div(5,"4"));
+console.log(div(5,""));
+console.log("floor");
+console.log(floor(-2.2,1));
+console.log(floor(2.2,1));
+console.log(floor(-1,true));
